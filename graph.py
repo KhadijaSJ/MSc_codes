1258163fa0b1b022d10b12691593807678fb9c13
KhadijaSJ <136462931+KhadijaSJ@users.noreply.github.com>
2023-06-13 14:28:26 +0200

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,5 +1,7 @@
-d1_up = 4194 #5039
-d1_down = 2723 #3541
+#Displays the up and down regulated differentially expressed genes (DEGS) per dataset as a bar plot.
+
+d1_up = 4194 
+d1_down = 2723 
 
 d3_up = 1731
 d3_down = 1936
@@ -31,3 +33,15 @@ plotdata.plot(kind="bar",figsize=(15, 8), color=['teal', 'tomato'])
 plt.xlabel("Datasets")
 
 plt.ylabel("Number of DEGs")
+
+
+#Venn diagram to show the intersection between datasets, given the values
+
+#Import libraries
+from matplotlib_venn import venn2, venn2_circles, venn2_unweighted
+from matplotlib_venn import venn3, venn3_circles
+from matplotlib import pyplot as plt
+%matplotlib inline
+
+venn2(subsets = (3607, 1144, 587), set_labels = ('Dataset 1', 'Dataset3'), set_colors=('red', 'teal'), alpha = 0.5)
+venn2_circles(subsets = (3607, 1144, 587), linestyle='dashed', linewidth= 1 , color='k')
